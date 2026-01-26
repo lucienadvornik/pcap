a7d05a0de797b00568cd0c98e3bc63910fb3b349
Lucie Nadvornik <lucka.prazakova@gmail.com>
2026-01-26 13:59:13 +0100

    Modules

---
+++ modules-path.py
@@ -0,0 +1,9 @@
+# import sys
+# print(sys.path)
+
+import sys
+sys.path.append('secret')
+import secret_module
+
+address = 'C:\\Users\\user\\..'
+address_for_one = '..\\secret' # one level up and look for folder secret
